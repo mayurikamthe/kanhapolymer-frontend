1c825aee47c3112830a462dc709d6f87a7bccee3
mayurikamthe <mayurikamthe200@example.com>
2025-11-30 17:09:41 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
 
-import Home from "./pages/Home/Home";
-import About from "./pages/About";
-import Shop from "./pages/Shop/Shop";
-import Contact from "./pages/Contact";
-import Faq from "./pages/Faq";
-import Error from "./pages/Error";
-
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <main className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        <Outlet />
       </main>
       <Footer />
-    </Router>
+    </>
   );
 }
 
